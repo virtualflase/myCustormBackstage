549c8aa18a3664fe88627262527957e4b745b32d
qinglang <ql.liang@qingkula.com>
2021-04-18 23:12:32 +0800

    做了基础basicLayout的布局

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,8 +4,19 @@ import styles from './index.css';
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
+      <div className={styles.collpaseMenu}>
+        这是侧边导航栏
+        </div>
+      <div className={styles.rightArea}>
+        <div className={styles.rightHeader}>
+          这是右边头部
+        </div>
+        <div>
+        {props.children}
+        </div>
+      </div>
+      {/* <h1 className={styles.title}>Yay! Welcome to umi!</h1> */}
+
     </div>
   );
 };
